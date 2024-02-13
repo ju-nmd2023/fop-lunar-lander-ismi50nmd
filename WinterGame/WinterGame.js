5f39b18b24d510904dcf43588c7a73a57e37d612
ismi50 <imis23fy@student.ju.se>
2024-02-13 19:40:19 +0100

    I adjusted the code a bit. Added the starting position for the spaceship, changed the velocity, and gravity. Now i just use the down key. I changed the canvas, added more snowflakes, drew the alien-spaceship, gravity. Added a costrain to make the spaceship not fly outside the canvas. Deleted the second & third iceberg, added if statements , added "YOU WIN" & "GAME OVER" text. Changed some widths and heights, added functions  mousePressed and KeyPressed.

--- WinterGame/WinterGame.js
+++ WinterGame/WinterGame.js
@@ -1,11 +1,26 @@
 let flakes = [];
 let gameActive = false;
+let spaceshipX = 138;
+let spaceshipY = 157;
+let spaceshipYVelocity = 1;
+let spaceshipYAccel = -0.3;
+let spaceshipXVelocity = 0;
+
+const iceberg1 = {
+  x: 67,
+  y: 540,
+  width: 90,
+  height: 142,
+};
+
+let gameOver = false;
+let win = false;
 
 function setup() {
-  createCanvas(1150, 690);
+  createCanvas(850, 700);
 
-  // Sowflakes
-  for (let i = 0; i < 250; i++) {
+  // Snowflakes
+  for (let i = 0; i < 350; i++) {
     const flake = {
       x: Math.floor(Math.random() * width),
       y: Math.floor(Math.random() * height),
@@ -17,11 +32,11 @@ function setup() {
 
 function draw() {
   if (gameActive) {
-    //Background
+    // Background
     noStroke();
     background(0, 0, 52);
 
-    //Snowflakes
+    // Snowflakes
     for (let flake of flakes) {
       fill(255, 255, 255, Math.abs(Math.sin(flake.alpha)) * 200);
       ellipse(flake.x, flake.y, 7.5);
@@ -34,7 +49,40 @@ function draw() {
       }
     }
 
-    // Design for the icebergs
+    // Move the spaceship forward
+    spaceshipX += spaceshipXVelocity;
+
+    // Head of the spaceship
+    fill(128, 128, 128);
+    ellipse(spaceshipX, spaceshipY, 40, 60);
+
+    // Body of the spaceship
+    fill(128, 128, 128);
+    ellipse(spaceshipX + 2, spaceshipY + 15, 150, 30);
+
+    // Black line on spaceship
+    stroke(0, 0, 0);
+    strokeWeight(2);
+    beginShape();
+    vertex(spaceshipX - 19, spaceshipY + 1);
+    bezierVertex(
+      spaceshipX - 14,
+      spaceshipY - 7,
+      spaceshipX + 11,
+      spaceshipY - 8,
+      spaceshipX + 20,
+      spaceshipY + 1
+    );
+    endShape();
+
+    // Gravity
+    spaceshipYVelocity += spaceshipYAccel;
+    spaceshipY += spaceshipYVelocity;
+
+    // To make the spaceship stay in the canvas
+    spaceshipY = constrain(spaceshipY, 60, height - 60);
+
+    // Design for the iceberg
     fill(255, 255, 255);
     stroke(255, 255, 255);
     strokeWeight(4);
@@ -45,17 +93,29 @@ function draw() {
     bezierVertex(107, 526, 168, 525, 210, 697);
     endShape();
 
-    // Second iceberg #2
-    beginShape();
-    vertex(409, 694);
-    bezierVertex(452, 240, 597, 239, 690, 696);
-    endShape();
+    if (
+      spaceshipX >= iceberg1.x &&
+      spaceshipX <= iceberg1.x + iceberg1.width &&
+      spaceshipY >= iceberg1.y - 2
+    ) {
+      if (spaceshipYVelocity < 0.7) {
+        win = true;
+      } else if (spaceshipYVelocity > 1.5) {
+        gameOver = true;
+      }
+      spaceshipYVelocity = 0;
+      spaceshipY = iceberg1.y;
+    }
 
-    // Third iceberg #3
-    beginShape();
-    vertex(928, 700);
-    bezierVertex(992, 213, 1065, 212, 1151, 697);
-    endShape();
+    if (gameOver) {
+      textSize(55);
+      fill(255, 0, 0);
+      text("GAME OVER!", width / 2 - 140, height / 2);
+    } else if (win) {
+      textSize(44);
+      fill(0, 255, 0);
+      text("YOU WIN!", width / 2 - 130, height / 2);
+    }
   } else {
     startScreen();
   }
@@ -67,20 +127,26 @@ function startScreen() {
   fill(255, 255, 255);
   textSize(50);
   textStyle(BOLD);
-  text("STARSHIP ICEBERG", 250, 180);
+  text("STARSHIP ICEBERG", 120, 220);
 
   // Second line of words
   fill(255, 0, 0);
-  textFont("Verdana");
-  textSize(17);
-  text("LAND THE STARSHIP ON THE MOUNTAIN", 330, 205);
+  textSize(20);
+  text("LAND THE STARSHIP ON THE MOUNTAIN", 170, 250);
 
   // Start
   fill(255, 255, 0);
   textSize(25);
-  text("Press Anywhere To Start!", 340, 450);
+  text("Press Anywhere To Start!", 220, 440);
 }
 
+// All functions
 function mousePressed() {
   gameActive = true;
+}
+
+function keyPressed() {
+  if (keyCode === DOWN_ARROW) {
+    spaceshipYVelocity = 8;
+  }
 }
